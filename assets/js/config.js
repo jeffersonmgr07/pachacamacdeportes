5a827e21008f52147e6750b940bb7e42806b4e65
jeffersonmgr07 <jefferson.mgr07@gmail.com>
2026-05-30 07:41:47 -0500

    Disable demo mode in app configuration

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  DEMO_MODE: true,
+  DEMO_MODE: false,
   API_URL: "", // Pega aquí la URL /exec de Apps Script cuando publiques el backend.
   CURRENT_ROUND: 3,
   CURRENT_CHAMPIONSHIP_ID: "CHAMP_FUT_MEN_2026"
