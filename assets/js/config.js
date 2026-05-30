71dffff381a3f3439a7058c18711f10589b0e32d
jeffersonmgr07 <jefferson.mgr07@gmail.com>
2026-05-30 02:21:45 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  DEMO_MODE: true,
+  DEMO_MODE: false,
   API_URL: "https://script.google.com/macros/s/AKfycbwj16ZVB7_7UjMNgLAtEHibXODGjBwjpMdZLVWT_183IGhLQC6I-NqC1XADWdPT4Xzs/exec", // Pega aquí la URL /exec de Apps Script cuando publiques el backend.
   CURRENT_ROUND: 3,
   CURRENT_CHAMPIONSHIP_ID: "CHAMP_FUT_MEN_2026"
