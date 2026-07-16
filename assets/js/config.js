6ec920f286b80c82f5f6c28b1cb7186d19fb0177
jeffersonmgr07 <jefferson.mgr07@gmail.com>
2026-07-16 13:14:17 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,5 +5,5 @@ window.APP_CONFIG = {
   CURRENT_CHAMPIONSHIP_ID: "CHAMP_FUT_MEN_2026",
   API_TIMEOUT_MS: 15000,
   // Pega aquí la URL del despliegue del Apps Script de alquiler de campos.
-  RENTALS_API_URL: ""
+  RENTALS_API_URL: "https://script.google.com/macros/s/AKfycbzCDb6Ea86s8w-bKv8pJdBsWWng7m4VFo0GzRWe0kPPtMclbg7HDtttM6VugbJZpQ9_Fw/exec"
 };
